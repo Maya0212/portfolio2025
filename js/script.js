8b880f4b4d2ea07cb3908ac5aacb6fd3f55e6dde
Maya0212 <mk233@hawaii.edu>
2025-09-06 00:15:43 -1000

    updated html

--- js/script.js
+++ js/script.js
@@ -24,6 +24,40 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 2000);
 });
 
+// SLIDE_ANIMATION //
+
+document.addEventListener("DOMContentLoaded", function () {
+  const targets = document.querySelectorAll(".slide-in-right");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target); // 一度表示したら監視を外す
+      }
+    });
+  }, { threshold: 0.2 });
+
+  targets.forEach(target => observer.observe(target));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll(".slide-in-left");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        // 一度だけ実行したい場合は↓
+        observer.unobserve(entry.target);
+      }
+    });
+  });
+
+  elements.forEach(el => observer.observe(el));
+});
+
+
 
 
 
